0892979bccbf7abb7324877cc83c9a7401e25e95
Zafer Celenk (ev) <zafercelenk@gmail.com>
2021-04-06 01:04:08 +0300

    travis deploy test 7

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 npm run build
-cd dist && tar cvfz ../dist.tar.gz . && cd ..
+cd build && tar cvfz ../build.tar.gz . && cd ..
 ssh-keyscan  -H $DEPLOY_IP >> ~/.ssh/known_hosts
-scp dist.tar.gz $DEPLOY_USER@$DEPLOY_IP:@DEPLOY_PATH
-ssh $DEPLOY_USER@$DEPLOY_IP "cd $DEPLOY_PATH && tar xvzf dist.tar.gz --overwrite"
-ssh $DEPLOY_USER@$DEPLOY_IP "cd $DEPLOY_PATH && rm -rf dist.tar.gz"
+scp build.tar.gz $DEPLOY_USER@$DEPLOY_IP:@DEPLOY_PATH
+ssh $DEPLOY_USER@$DEPLOY_IP "cd $DEPLOY_PATH && tar xvzf build.tar.gz --overwrite"
+ssh $DEPLOY_USER@$DEPLOY_IP "cd $DEPLOY_PATH && rm -rf build.tar.gz"
